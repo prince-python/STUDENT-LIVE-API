bffd1acea42de0c63df1f98e68df3f08704bada7
kali <kali@kali>
2023-04-27 14:19:38 -0400

    first commit

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from . models import *
+
+
+admin.site.register(Course)
+admin.site.register(AddStudents)
+admin.site.register(Registration)
+admin.site.register(Teacher)
